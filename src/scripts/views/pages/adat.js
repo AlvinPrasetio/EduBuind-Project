a20d2a4885226894887f3e7889592a3b071fb0d6
Alvin Prasetio <alvinprazz11@gmail.com>
2024-06-16 21:25:06 +0700

    Update Kontent adat dan pakaian

--- src/scripts/views/pages/adat.js
+++ src/scripts/views/pages/adat.js
@@ -1,10 +1,25 @@
+import adatData from '../../../DATA/adat.json';
+
 const Adat = {
   async render() {
+    const adatList = adatData.adat;
     return `
     <div class="container">
       <div class="title">
         <h1>Adat Istiadat di Indonesia berdasarkan Provinsi</h1>
       </div>
+      ${adatList.map((adat) => `
+        <h2>Provinsi ${adat.province}</h2>
+        <div class="card-container">
+            <div class="card-content">
+              <h3>${adat.name}</h3>
+                <img src="${adat.pictureId}" alt="${adat.name}">
+            </div>
+        </div>
+        <div class="description-content">
+            <p>${adat.description}</p>
+        </div>
+      `).join('')}
     </div>
         `;
   },
--- src/scripts/views/pages/pakaian.js
+++ src/scripts/views/pages/pakaian.js
@@ -1,10 +1,25 @@
+import pakaianData from '../../../DATA/pakaian.json';
+
 const Pakaian = {
   async render() {
+    const pakaianList = pakaianData.pakaian;
     return `
     <div class="container">
       <div class="title">
         <h1>Pakaian Adat di Indonesia berdasarkan Provinsi</h1>
       </div>
+      ${pakaianList.map((pakaian) => `
+        <h2>Provinsi ${pakaian.province}</h2>
+        <div class="card-container">
+            <div class="card-content">
+              <h3>${pakaian.name}</h3>
+                <img src="${pakaian.pictureId}" alt="${pakaian.name}">
+            </div>
+        </div>
+        <div class="description-content">
+            <p>${pakaian.description}</p>
+        </div>
+      `).join('')}
     </div>
         `;
   },
